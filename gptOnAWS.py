84ca9e1bad66bec05376a7e687e241c6c9076bd7
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-04-29 20:30:31 +0100

    add avx instructions

--- gptOnAWS.py
+++ gptOnAWS.py
@@ -1,5 +1,5 @@
 from gpt4allj import Model
 
-model = Model('./models/ggml-gpt4all-j.bin')
+model = Model('./models/ggml-gpt4all-j.bin', instructions='avx')
 
 print(model.generate('AI is going to'))
