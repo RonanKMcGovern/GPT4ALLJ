e01b45b591bc1c1e46f96adc9e7dc241e31e0143
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-04-29 20:37:27 +0100

    adjust prompt

--- gptOnAWS.py
+++ gptOnAWS.py
@@ -2,4 +2,4 @@ from gpt4allj import Model
 
 model = Model('./models/ggml-gpt4all-j.bin', instructions='avx')
 
-print(model.generate('AI is going to'))
+print(model.generate('What are the strengths and weaknesses of AWS versus Google Cloud?'))
