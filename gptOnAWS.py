e6fa82621430c2a47bd0a5832d9cd62503eb8ba5
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-04-30 11:24:36 +0100

    remove avx instruction as avx2 is default

--- gptOnAWS.py
+++ gptOnAWS.py
@@ -1,5 +1,5 @@
 from gpt4allj import Model
 
-model = Model('./models/ggml-gpt4all-j.bin', instructions='avx')
+model = Model('./models/ggml-gpt4all-j.bin')
 
 print(model.generate('What are the strengths and weaknesses of AWS versus Google Cloud?'))
