9b498b496105c13d02c534c882a0a7987baad864
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-04-30 16:50:03 +0100

    add 8 threads

--- gptOnAWS.py
+++ gptOnAWS.py
@@ -1,5 +1,5 @@
 from gpt4allj import Model
 
-model = Model('./models/ggml-gpt4all-j.bin')
+model = Model('./models/ggml-gpt4all-j.bin', n_threads=8)
 
 print(model.generate('What are the strengths and weaknesses of AWS versus Google Cloud?'))
