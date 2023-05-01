afc5b3557f035d7ccc98a826525fac19627b52d2
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-05-01 17:50:57 +0100

    back to just a text input

--- testRequest.py
+++ testRequest.py
@@ -6,7 +6,7 @@ load_dotenv()  # Load environment variables from .env file
 server_ip = os.getenv('SERVER_IP')
 
 url = f'http://{server_ip}:8000/complete'
-data = {'text': 'Write a very short', 'completionLength': 300}
+data = {'text': 'Write a very short'}
 response = requests.post(url, json=data)
 
 print(response.json())  # Print the JSON response from the server
